d3648eda8268e1f5afd731967836ea9217a5333c
ALiArgabi <40006923+ALiArgabi@users.noreply.github.com>
2019-09-13 21:10:20 +0300

    first_flask_mysql | added Validation

--- python_stack/flask/flask_fundamentals/Assignmenta/DojoSurvey/runServer.py
+++ python_stack/flask/flask_fundamentals/Assignmenta/DojoSurvey/runServer.py
@@ -1,7 +1,8 @@
 # Assignment: Dojo Survey
 
-from flask import Flask, render_template, request, redirect  # added request
+from flask import Flask, render_template, request, redirect ,flash,session # added request
 app = Flask(__name__)
+app.secret_key="ali secret_key"
 # our index route will handle rendering our form
 @app.route('/')
 def index():
@@ -10,15 +11,23 @@ def index():
 
 @app.route('/result', methods=['POST'])
 def sendButton():    
+    if len(request.form['yourName'])<1:
+        flash("Please enter your name")
+    if len(request.form['DojoLocation'])<1:
+        flash("Please selecte Dojo's location")
+    if len(request.form['FacoriteLanguage'])<1:
+        flash("Please selecte your Favorite Language")
 
     rf_showName = request.form['yourName']
-
     rf_showDojoLocation = request.form['DojoLocation']
     rf_showFavoriteLanguage = request.form['FacoriteLanguage']
     rf_showComment = request.form['Comment']
 
-    return render_template("resultPage.html", showName=rf_showName, showDojoLocation =rf_showDojoLocation ,showFavoriteLanguage=rf_showFavoriteLanguage, showComment=rf_showComment)
-
+    if  '_flashes' in session.keys():	# no flash messages means all validations passed
+        return redirect("/")
+    else:
+        return render_template("resultPage.html", showName=rf_showName, showDojoLocation =rf_showDojoLocation ,showFavoriteLanguage=rf_showFavoriteLanguage, showComment=rf_showComment)
+   
 
 if __name__ == "__main__":
     app.run(debug=True)
