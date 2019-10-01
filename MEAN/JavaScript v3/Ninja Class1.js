0d549d9f956f316df4a6f0ca3d4ba9296bb6f0e4
ALiArgabi <40006923+ALiArgabi@users.noreply.github.com>
2019-10-01 06:34:36 +0300

    Assignment: Ninja Class1 - done

---
+++ MEAN/JavaScript v3/Ninja Class1.js
@@ -0,0 +1,28 @@
+//Assignment: Ninja Class 1
+
+function Ninja (name){
+
+    this.name = name;
+    this.health= 100;
+
+    //private varaibles
+    var speed =3
+    var strenght =3
+
+//methods
+this.sayName = function(){ console.log("My ninja name is "+this.name+"!"); };
+
+this.showStats = function(){ console.log("Name: "+this.name+", Health: "+this.health+
+", Speed: "+speed+", Strength: "+speed); };
+
+// This should add +10 Health to the Ninja
+this.drinkSake = function() {this.health+=10};
+
+}
+
+var ninja1 = new Ninja("Hyabusa");
+ninja1.sayName();
+ninja1.showStats();
+ninja1.drinkSake()
+ninja1.showStats();
+
