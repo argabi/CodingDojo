92c399a54b22cc1b930c4e08b783d6f9723e0436
Aargabi <Aargabi@site.sa>
2020-02-04 14:13:42 +0300

    random nuymbers

--- C#/FirstCSharp/Program.cs
+++ C#/FirstCSharp/Program.cs
@@ -2,11 +2,14 @@
 
 namespace FirstCSharp
 {
+    
     class Program
     {
+       
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+             Random rand = new Random();
+            Console.WriteLine(rand.Next(2,8));
         }
     }
 }
